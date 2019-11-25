3b42dd0ea0a4d6be91f1294fc3b0c33be1a05e1c
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-25 03:44:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,9 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :title
+
+  def initialize(title)
+      @title = title
+  end
+end
